29bb8d53daafb948445799187ced7dfe403117a2
mplowey28 <matthew.plowey@gmail.com>
2021-01-11 15:34:30 +0100

    added tailwind style

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -5,6 +5,7 @@ const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
     <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       onClick={() =>
         loginWithRedirect({
           screen_hint: "signup",
